85034d994356ac663daeb582e8d767be259a2a97
seanwhs <54506937+seanwhs@users.noreply.github.com>
2026-05-31 21:06:33 +0800

    Create variables.js

---
+++ 01-foundations-js/01-variables/variables.js
@@ -0,0 +1,14 @@
+const courseName = 'Web Journey Blueprint';
+let studentCount = 0;
+
+studentCount += 1;
+
+console.log(courseName);
+console.log(studentCount);
+
+const user = {
+  name: 'Sean',
+  role: 'Student'
+};
+
+console.log(user);

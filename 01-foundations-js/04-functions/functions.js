8c74d46839baf98291e53eddb338a12f6fa5e3f5
seanwhs <54506937+seanwhs@users.noreply.github.com>
2026-06-01 06:38:27 +0800

    Update functions.js

--- 01-foundations-js/04-functions/functions.js
+++ 01-foundations-js/04-functions/functions.js
@@ -1,13 +1,31 @@
+// 01-foundations-js/04-functions/functions.js
+
+/**
+ * 1. Function Declaration
+ * Hoisted (can be called before it's defined in the code).
+ */
 function greet(name) {
   return `Hello ${name}`;
 }
 
+/**
+ * 2. Function Expression (Arrow Function)
+ * Not hoisted. Uses a standard block body.
+ * Great for multi-line logic.
+ */
 const greetArrow = name => {
   return `Welcome ${name}`;
 };
 
+/**
+ * 3. Implicit Return Arrow Function
+ * The most concise way to write a function.
+ * If there is only one expression, the 'return' keyword 
+ * and curly braces are optional.
+ */
 const shortGreeting = name => `Hi ${name}`;
 
+// Execution
 console.log(greet('Sean'));
 console.log(greetArrow('Alex'));
 console.log(shortGreeting('Taylor'));

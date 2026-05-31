4fbd77bbfb93f5a5a85e073a88ff6ee836922fcd
seanwhs <54506937+seanwhs@users.noreply.github.com>
2026-06-01 07:55:23 +0800

    Create while.js

---
+++ 01-foundations-js/12-loops/while.js
@@ -0,0 +1,10 @@
+// 01-foundations-js/12-loops/while.js
+// Check First
+
+let count = 0;
+
+// Checks 'count < 3' before executing the console.log
+while (count < 3) {
+  console.log(`Count is: ${count}`);
+  count++;
+}

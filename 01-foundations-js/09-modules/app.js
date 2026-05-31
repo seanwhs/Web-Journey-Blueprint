7d71f5e60d046dad243ab5cead3a71058ab04616
seanwhs <54506937+seanwhs@users.noreply.github.com>
2026-06-01 07:17:34 +0800

    Update app.js

--- 01-foundations-js/09-modules/app.js
+++ 01-foundations-js/09-modules/app.js
@@ -1,4 +1,5 @@
+// Importing specific functions from the './math.js' file
 import { add, subtract } from './math.js';
 
-console.log(add(5, 3));
-console.log(subtract(10, 4));
+console.log(add(5, 3));      // Output: 8
+console.log(subtract(10, 4)); // Output: 6

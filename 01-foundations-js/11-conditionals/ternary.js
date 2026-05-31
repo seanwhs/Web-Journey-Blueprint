19f6630060e26cc911b754414d603c1d8d71a3ab
seanwhs <54506937+seanwhs@users.noreply.github.com>
2026-06-01 07:44:39 +0800

    Create ternary.js

---
+++ 01-foundations-js/11-conditionals/ternary.js
@@ -0,0 +1,12 @@
+// 01-foundations-js/11-conditionals/ternary.js
+
+// Syntax: condition ? value_if_true : value_if_false
+
+const age = 20;
+
+const status = 
+  age >= 18 
+    ? 'Adult'   // Logic executed if true
+    : 'Minor';  // Logic executed if false
+
+console.log(status); // Output: 'Adult'

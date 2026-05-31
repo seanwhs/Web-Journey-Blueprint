01108806c4a7dc49bb04f99f2f0bab74d9b85d5a
seanwhs <54506937+seanwhs@users.noreply.github.com>
2026-06-01 07:19:24 +0800

    Update math.js

--- 01-foundations-js/09-modules/math.js
+++ 01-foundations-js/09-modules/math.js
@@ -1,3 +1,8 @@
-export const add = (a, b) => a + b;
+// 01-foundations-js/09-modules/math.js
 
+// 'export' keyword defines this as part of the ESM interface. 
+// ESM uses a 'live binding' system, meaning this file provides a direct 
+// reference to the functions rather than a static copy.
+
+export const add = (a, b) => a + b;
 export const subtract = (a, b) => a - b;

ee459dec6c507235351267f424edf033b6800fb4
seanwhs <54506937+seanwhs@users.noreply.github.com>
2026-05-31 21:09:30 +0800

    Create destructuring.js

---
+++ 01-foundations-js/06-destructuring/destructuring.js
@@ -0,0 +1,17 @@
+const user = {
+  name: 'Sean',
+  age: 30,
+  country: 'Singapore'
+};
+
+const { name, age } = user;
+
+console.log(name);
+console.log(age);
+
+const colors = ['red', 'blue', 'green'];
+
+const [primary, secondary] = colors;
+
+console.log(primary);
+console.log(secondary);

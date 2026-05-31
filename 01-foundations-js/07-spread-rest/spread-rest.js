90a5ff7947d443f6616d49e55b7843994a63b566
seanwhs <54506937+seanwhs@users.noreply.github.com>
2026-06-01 07:08:53 +0800

    Update spread-rest.js

--- 01-foundations-js/07-spread-rest/spread-rest.js
+++ 01-foundations-js/07-spread-rest/spread-rest.js
@@ -1,17 +1,21 @@
+// 01-foundations-js/07-spread-rest/spread-rest.js
+
+/**
+ * Spread Syntax: Expanding elements
+ * Rest Syntax: Collecting elements
+ */
+
+// 1. Spread Operator (...): Expands an array into individual elements
 const todos = ['React', 'TypeScript'];
+const updatedTodos = [...todos, 'Next.js']; 
+// Essentially: ['React', 'TypeScript', 'Next.js']
 
-const updatedTodos = [
-  ...todos,
-  'Next.js'
-];
+console.log(updatedTodos); // ['React', 'TypeScript', 'Next.js']
 
-console.log(updatedTodos);
-
+// 2. Rest Parameter (...): Collects multiple arguments into a single array
 const sum = (...numbers) => {
-  return numbers.reduce(
-    (total, number) => total + number,
-    0
-  );
+  // 'numbers' is now an array: [1, 2, 3, 4]
+  return numbers.reduce((total, number) => total + number, 0);
 };
 
-console.log(sum(1, 2, 3, 4));
+console.log(sum(1, 2, 3, 4)); // 10

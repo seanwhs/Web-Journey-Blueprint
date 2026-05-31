2f77acfa561c0b277426834239cbb3128cb2e471
seanwhs <54506937+seanwhs@users.noreply.github.com>
2026-05-31 21:15:22 +0800

    Create App.jsx

---
+++ 02-react-basics/src/App.jsx
@@ -0,0 +1,17 @@
+import Counter from './components/Counter';
+import TodoList from './components/TodoList';
+import UserList from './components/UserList';
+
+const App = () => {
+  return (
+    <main>
+      <h1>React Fundamentals</h1>
+
+      <Counter />
+      <TodoList />
+      <UserList />
+    </main>
+  );
+};
+
+export default App;
